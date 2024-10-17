9cf894151f60f820c95c31491476911d716c3423
pkrishnavamsi0709 <pkrishnavamsi2001@gmail.com>
2024-10-17 17:47:00 +0530

    Banner Changes
    added the integration for Banner

--- components/render-components.tsx
+++ components/render-components.tsx
@@ -1,29 +1,10 @@
-import React from "react";
-
-import HeroBanner from "./hero-banner";
-import BlogBanner from "./blog-banner";
-import SectionWithHtmlCode from "./section-with-html-code";
-import GalleryImagesSection from "./gallery";
 import { RenderProps } from "../typescript/component";
-import SpecialsSection from "./specials";
-import ContactUsSection from "./contactus";
-import MenuSection from "./menu";
-import BookingSection from "./booking";
-import EventsSection from "./events";
-import AboutSection from "./about";
-import TestimonialSection from "./testimonial";
-import WhyUsSection from "./why_us";
-import ChefsSection from "./chef";
 import Banner from "./banner";
-import Register from "./register";
-import ItmaBlogNews from "./itma-blog-news";
-import JoinItma from "./join-itma";
 import Brands from "./brands";
 import ITMAConnect from "./itma-connect";
 
 export default function RenderComponents(props: RenderProps) {
   const { pageComponents, blogPost, entryUid, contentTypeUid, locale } = props;
-  console.log("pageComponents", pageComponents)
   return (
     <div
       data-pageref={entryUid}
@@ -32,26 +13,18 @@ export default function RenderComponents(props: RenderProps) {
     >
       {pageComponents?.map((component, key: number) => {
         if (component.banner) {
-          return (
-            <Banner
-              key={`component-${key}`}
-            />
-          );
-        } 
+          return <Banner section={component.banner} key={`component-${key}`} />;
+        }
         if (component.register) {
-          return (
-            <Register
-            key={`component-${key}`}
-          />
-          );
-        } 
+          return <Register key={`component-${key}`} />;
+        }
         if (component["itma-blogs-and-news"]) {
           return (
             <ItmaBlogNews
             key={`component-${key}`}
           />
-          );
-        } 
+          return <ItmaBlogNews key={`component-${key}`} />;
+        }
 
         if (component["itma-connect"]) {
           return (
