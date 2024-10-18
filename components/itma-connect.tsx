ba97315d868106819a4e36ef22e2422d4abf1541
parvathisomanahallicnx <parvathi.somanahalli@concentrix.com>
2024-10-18 16:17:37 +0530

    ITMA Connect html and css changes
    ITMA Connect html and css changes

--- components/itma-connect.tsx
+++ components/itma-connect.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { Image, Action } from "../typescript/action";
 import { useState } from "react";
+import BlogIcon from '../assets/svg/blog-icon.svg';
   
   type ITMAConnectProps = {
     title: string;
@@ -12,15 +13,40 @@ import { useState } from "react";
 
   export default function ITMAConnect({ section }: { section: ITMAConnectProps }) {
     return (
-      <div id = "itma-connect">
-      <p style={{color: "black"}}>{section?.title}</p>
-      <p style={{color: "black"}}>{section?.heading}</p>
-      <p style={{color: "black"}}>{section?.description}</p>
-      <img
-            src={section?.image.url}
-            className=""
-            alt=""
-          />
-      </div>
+      <section id="itma-connect">
+           <div className="connect-text">
+            <div className="connect-icon">
+              <img src={BlogIcon} />
+            </div>
+            <div className="connect-title">
+              <span  className="connect-title-text">{section?.title}</span>
+            </div>
+            <div className="connect-heading">
+              <span  className="connect-heading-text">{section?.heading}</span>
+            </div>
+            <div className="connect-description">
+              <span  className="connect-description-text">{section?.description}</span>
+            </div>
+          </div>
+          <div className="connect-image-container">
+            <div className="connect-image">
+              <img
+                src={section?.image.url}
+                className="image-div bg-no-repeat fill-parent bg-cover nodeBg-872716"
+                alt=""
+              />
+            </div>
+            <div className="connect-firrplay-icon">
+              <div className="connect-vector">
+                <div
+                  className="connect-nodeBg fill-parent image-div bg-contain bg-no-repeat"
+                ></div>
+              </div>
+            </div>
+          </div>
+          <div className="connect-learn-more">
+            <span className="connect-learn-more-text ">Learn More</span>
+          </div>
+      </section>
     );
   }
