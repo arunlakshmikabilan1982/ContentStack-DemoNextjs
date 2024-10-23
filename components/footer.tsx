963de69f314dc2617df395faefeda9d6a126b013
pkrishnavamsi0709 <pkrishnavamsi2001@gmail.com>
2024-10-23 14:23:42 +0530

    changes in footer
    added the missing key prop in footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FooterProps } from "../typescript/layout";
 
 export default function Footer({
-  footerSection
+  footerSection,
 }: {
   footerSection: FooterProps;
 }) {
@@ -12,15 +12,27 @@ export default function Footer({
         <div className="footer-a ">
           <div className="footer-logo-container ">
             <div className="footer-logo-name ">
-              <img src={footerSection?.logo?.url} alt="logo" width="38px" height="32px" />
+              <img
+                src={footerSection?.logo?.url}
+                alt="logo"
+                width="38px"
+                height="32px"
+              />
               <span className="itma-2027">{footerSection?.title}</span>
             </div>
-            <span className="followus">{footerSection?.social.social_share_title}</span>
+            <span className="followus">
+              {footerSection?.social.social_share_title}
+            </span>
             <div className="footericons">
-              {footerSection?.social.social_share.map((share) => {
+              {footerSection?.social.social_share.map((share, i) => {
                 return (
-                  <a href={share.link.href}>
-                    <img src={share.icon.url} alt={share.link.title} width="32px" height="32px" />
+                  <a href={share.link.href} key={i}>
+                    <img
+                      src={share.icon.url}
+                      alt={share.link.title}
+                      width="32px"
+                      height="32px"
+                    />
                   </a>
                 );
               })}
@@ -28,13 +40,17 @@ export default function Footer({
           </div>
           <div className="footer-menu-a ">
             <div className="about-us ">
-              <span className="about-us-0 ">{footerSection?.about_us.heading}</span>
+              <span className="about-us-0 ">
+                {footerSection?.about_us.heading}
+              </span>
             </div>
             <div className="menu-a-content ">
-              {footerSection?.about_us.link.map((linkData) => {
+              {footerSection?.about_us.link.map((linkData, i) => {
                 return (
-                  <div className="about-itma-872809 ">
-                    <a className="about-itma-872809-0 " href={linkData.href}>{linkData.title}</a>
+                  <div className="about-itma-872809 " key={i}>
+                    <a className="about-itma-872809-0 " href={linkData.href}>
+                      {linkData.title}
+                    </a>
                   </div>
                 );
               })}
@@ -42,13 +58,17 @@ export default function Footer({
           </div>
           <div className="footer-menu-a ">
             <div className="about-us ">
-              <span className="about-us-0 ">{footerSection?.exhibitors_visitors.heading}</span>
+              <span className="about-us-0 ">
+                {footerSection?.exhibitors_visitors.heading}
+              </span>
             </div>
             <div className="menu-a-content ">
-              {footerSection?.exhibitors_visitors.link.map((linkData) => {
+              {footerSection?.exhibitors_visitors.link.map((linkData, i) => {
                 return (
-                  <div className="about-itma-872809 ">
-                    <a className="about-itma-872809-0 " href={linkData.href}>{linkData.title}</a>
+                  <div className="about-itma-872809 " key={i}>
+                    <a className="about-itma-872809-0 " href={linkData.href}>
+                      {linkData.title}
+                    </a>
                   </div>
                 );
               })}
@@ -56,13 +76,17 @@ export default function Footer({
           </div>
           <div className="footer-menu-a ">
             <div className="about-us ">
-              <span className="about-us-0 ">{footerSection?.media_resources.heading}</span>
+              <span className="about-us-0 ">
+                {footerSection?.media_resources.heading}
+              </span>
             </div>
             <div className="menu-a-content ">
-              {footerSection?.media_resources.link.map((linkData) => {
+              {footerSection?.media_resources.link.map((linkData, i) => {
                 return (
-                  <div className="about-itma-872809 ">
-                    <a className="about-itma-872809-0 " href={linkData.href}>{linkData.title}</a>
+                  <div className="about-itma-872809 " key={i}>
+                    <a className="about-itma-872809-0 " href={linkData.href}>
+                      {linkData.title}
+                    </a>
                   </div>
                 );
               })}
@@ -70,13 +94,17 @@ export default function Footer({
           </div>
           <div className="footer-menu-a ">
             <div className="about-us ">
-              <span className="about-us-0 ">{footerSection?.support.heading}</span>
+              <span className="about-us-0 ">
+                {footerSection?.support.heading}
+              </span>
             </div>
             <div className="menu-a-content ">
-              {footerSection?.support.link.map((linkData) => {
+              {footerSection?.support.link.map((linkData, i) => {
                 return (
-                  <div className="about-itma-872809 ">
-                    <a className="about-itma-872809-0 " href={linkData.href}>{linkData.title}</a>
+                  <div className="about-itma-872809 " key={i}>
+                    <a className="about-itma-872809-0 " href={linkData.href}>
+                      {linkData.title}
+                    </a>
                   </div>
                 );
               })}
@@ -85,10 +113,12 @@ export default function Footer({
         </div>
         <div className="footer-b ">
           <div className="footer-b-content">
-            {footerSection?.navigation.link.map((linkData) => {
+            {footerSection?.navigation.link.map((linkData, i) => {
               return (
-                <div className="privacy-notice-872832 ">
-                  <a href={linkData.href} className="privacy-notice-872832-0 ">{linkData.title}</a>
+                <div className="privacy-notice-872832 " key={i}>
+                  <a href={linkData.href} className="privacy-notice-872832-0 ">
+                    {linkData.title}
+                  </a>
                 </div>
               );
             })}
